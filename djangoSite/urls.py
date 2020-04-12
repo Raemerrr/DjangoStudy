4fb24a9c3ecd52c98e07edf833adad0c9d2d1d2b
Raemin KANG <foals18@gmail.com>
2020-04-12 21:25:08 +0900

    polls 업로드 더 힘내자..

--- djangoSite/urls.py
+++ djangoSite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path 
 
 urlpatterns = [
+path(r'polls/',include('polls.urls')),
 path(r'admin/', admin.site.urls),
 path(r'', include('raemin.urls')), #raemin app을 include 해주는것임.
 ]
